f77c13577f4641f5e3d07b12fb3b68aaef18f356
eviDim <evi_dimitropoulou@yahoo.gr>
2016-05-30 11:06:58 +0300

    Add files via upload

---
+++ Helpers.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AegeanProject
+{
+    public static class Helpers
+    {
+        public static bool IsFileValidImage(this HttpPostedFileBase file)
+        {
+            if (file == null)
+                return true;
+            if (file.ContentLength <= 0)
+            {
+
+                return false;
+
+            }
+            
+            return file.ContentType == "image/png" || file.ContentType == "image/jpeg";
+        }
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,16 @@
+﻿using System.Web.Http;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(AegeanProject.Startup))]
+namespace AegeanProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+            
+        }
+    }
+}
